f141f46ae07fc4d496d6aeb1fb490748b2a3a34c
chriscivetta <cccivetts@gmail.com>
2020-02-25 08:42:29 -0500

    Created a button and label and added them to the window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include <QtWidgets>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QWidget window;
+    window.resize(320, 240);
+    window.show();
+    window.setWindowTitle(
+            QApplication::translate("topLevel", "Window"));
+
+    QPushButton *button = new QPushButton(
+            QApplication::translate("childWidget", "Press me"), &window);
+    button->move(100, 100);
+    button->show();
+
+
+    QLabel *label = new QLabel(
+            QApplication::translate("childWidget", "0"), &window);
+    label->move(150, 150);
+    label->show();
+
+    return app.exec();
 }
